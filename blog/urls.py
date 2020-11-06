7eccb0e6ee918fe8916b0c5108012aed60b3d42a
Hassan <hassanalihazaraa@gmail.com>
2020-11-06 18:31:34 +0100

    done

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView, PostDetail, PostNew, PostUpdate, PostDelete
+)
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/full', views.post_full_detail, name='post_full_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('post/<int:pk>/full', PostDetail.as_view(), name='post_full_detail'),
+    path('post/new/', PostNew.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', PostUpdate.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,48 +1,71 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+# Display all the post list
+class PostListView(ListView):
+    template_name = 'blog/post_list.html'
+    model = Post
+    context_object_name = 'queryset'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['published_date'] = Post.objects.all().order_by('published_date')
+        return context
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+# Show post detail
+class PostDetail(DetailView):
+    template_name = 'blog/post_detail.html'
+    model = Post
+    context_object_name = 'post'
 
 
-def post_full_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+# Add a post
+class PostNew(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create'
+        return context
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.published_date = timezone.now()
+        form.save()
+        return redirect(reverse("post_detail", kwargs={
+            'pk': form.instance.pk
+        }))
 
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+# Update a post
+class PostUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update'
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return redirect(reverse("post_detail", kwargs={
+            'pk': form.instance.pk
+        }))
 
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+# Delete a post
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'blog/post_delete.html'
